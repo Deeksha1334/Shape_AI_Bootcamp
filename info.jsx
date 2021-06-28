fd8de635c1f7d5d7e594d5615ccd8e4d1a088397
Deeksha1334 <84957410+Deeksha1334@users.noreply.github.com>
2021-06-28 22:19:53 +0530

    Add files via upload

---
+++ info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info(){
+return {
+  <div className ="note">
+<h1> Javascipt and React.js<h1>
+<p> A Basic Wev Dev React Js Bootcamp</p>
+</div>
+};
+}
+
+export default Info;
